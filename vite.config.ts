822277602b7bdae5400fb17a4dd7e1988e94870c
wael-ben-salem <108833476+wael-ben-salem@users.noreply.github.com>
2025-06-20 18:47:51 +0100

    Déploiement GitHub Pages

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/wael-portfolio/' : '/',
-    plugins: [react()],
-  }
-});
